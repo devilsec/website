1110f0c4de01e1939d4897390455bdcd237312b9
Daylam Tayari <git@tayari.gg>
2022-04-19 17:29:04 -0700

    Implemented loading of HTML pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,38 @@ const path=require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-let files=[];
-fs.readdirSync('./src/pages/').forEach(file => {
-    if (path.extname(file)=='.html'){
-        files.push(file.split('.')[0]);
-    }
-});
-
-let multipleHtmlPlugins=files.map(file => {
-    return new HtmlWebpackPlugin({
-        template: `./src/pages/${file}.html`,
-        filemame: `${file}.html`,
-        favicon: './src/assets/img/favicon.ico',
-        chunks: ['main', `${file}`],
-        meta: {
-            viewport: 'width=device-width, initial-scale=1',
+function generateHTML(){
+    const pages=fs.readdirSync(path.resolve(__dirname, './src/pages'))
+    pages.unshift('index.html');
+    const htmlPages=pages.filter(page => path.extname(page)=='.html');
+    return htmlPages.map(item => {
+        const name=item.split('.')[0]
+        if(name=='index'){
+            return new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+                favicon: './src/assets/img/favicon.ico',
+                chunks: ['main'],
+                meta: {
+                    viewport: 'width=device-width, initial-scale=1',
+                }
+            })
         }
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: `./src/pages/${name}.html`,
+            favicon: './src/assets/img/favicon.ico',
+            chunks: [`${name}`],
+            meta: {
+                viewport: 'width=device-width, initial-scale=1',
+            }
+        })
     })
-});
+}
+const generatedHTML=generateHTML();
 
 module.exports={
-    entry: {
-        main: './src/components/header.js'
-    },
+    entry: './src/index.js',
     mode: 'development',
     output:{
         path: path.resolve(__dirname, './dist/'),
@@ -60,14 +69,5 @@ module.exports={
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            favicon: './src/assets/img/favicon.ico',
-            chunks: ['main'],
-            meta: {
-                viewport: 'width=device-width, initial-scale=1',
-            }
-        })
-    ].concat(multipleHtmlPlugins)
+    ].concat(generatedHTML)
 }
