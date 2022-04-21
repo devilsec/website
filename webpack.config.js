bca6b25cfb753cab65f2bf5cb611c7e23d846809
Daylam Tayari <git@tayari.gg>
2022-04-21 05:31:51 -0700

    Implemented and fixed subcomponent loading
    Fixed the loading of JS components, including web components. Fixed the loading of CSS and SCSS stylesheets. Created functions to automatically add all scss and js files for that respective page in the entry points to then be loaded with the page. Added loading for images and vectors. Improved loading for HTML pages. Implemented loading of JS components with babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path=require('path');
 const fs = require('fs');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let entryPoints = {};
+
+function generateEntryPoints(name){
+    let entries=[];
+    if(fs.existsSync(`./src/assets/scss/${name}.scss`)){
+        entries.push(`./src/assets/scss/${name}.scss`);
+    }
+    if(fs.existsSync(`./src/assets/js/${name}.js`)){
+        entries.push(`./src/assets/js/${name}.js`);
+    }
+    if(entries.length>0){
+        entryPoints[name]=entries;
+    }
+}
 
 function generateHTML(){
     const pages=fs.readdirSync(path.resolve(__dirname, './src/pages'))
@@ -9,6 +25,7 @@ function generateHTML(){
     return htmlPages.map(item => {
         const name=item.split('.')[0]
         if(name=='index'){
+            entryPoints['main']='./src/index.js';
             return new HtmlWebpackPlugin({
                 template: './src/index.html',
                 filename: 'index.html',
@@ -19,11 +36,12 @@ function generateHTML(){
                 }
             })
         }
+        generateEntryPoints(name);
         return new HtmlWebpackPlugin({
             filename: `${name}.html`,
             template: `./src/pages/${name}.html`,
             favicon: './src/assets/img/favicon.ico',
-            chunks: [`${name}`],
+            chunks: ['main', name],
             meta: {
                 viewport: 'width=device-width, initial-scale=1',
             }
@@ -33,7 +51,7 @@ function generateHTML(){
 const generatedHTML=generateHTML();
 
 module.exports={
-    entry: './src/index.js',
+    entry: entryPoints,
     mode: 'development',
     output:{
         path: path.resolve(__dirname, './dist/'),
@@ -42,20 +60,22 @@ module.exports={
     module: {
         rules: [
             {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
                 test: /\.(scss)$/,
                 use: [
                     {loader: 'style-loader'},
                     {loader: 'css-loader'},
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function(){
-                                return[
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    },
+                    {loader: 'postcss-loader'},
                     {loader: 'sass-loader'}
                 ]
             },
@@ -65,6 +85,10 @@ module.exports={
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
         ]
     },
