00a270b2583030804bbefc0938482360bdcdc85e
Prithvi <prithviraj645@gmail.com>
2022-10-20 20:05:09 -0700

    Added functionality to parse the json files in videos index and build the content for the videos page. The find_full_element function still needs work as it doesn't work as expected yet.

--- src/videosgen.py
+++ src/videosgen.py
@@ -11,13 +11,16 @@ __copyright__ = 'Copyright (c) 2022 Prithviraj Renjella Rajendra Prasad'
 __license__ = 'GNU GPL v3'
 
 # Imports
+from contextlib import closing
+from glob import glob
 import re
+from tracemalloc import start
 import requests
 import json
 from datetime import datetime
 
-# Retrieve Indexes as Parsed JSON:
 
+# Retrieve Indexes as Parsed JSON:
 def retrieve_index_json(name):
     return json.loads(requests.get(f"https://raw.githubusercontent.com/devilsec/videos-index/master/{name}.json").text)
 
@@ -29,17 +32,25 @@ tags_index = retrieve_index_json('tags').get('tags')
 
 html = open("./dist/videos.html", "r").read()
 
-def find_full_div(start_pos, last_div_pos):
+# this has a bug
+def find_full_element(element_name, start_pos, last_div_pos):
     global html
-    next_div_pos = html.find('</div>', last_div_pos)+6
-    start_div_end_pos = html.find('>', start_pos)+1
-    div_contents = html[start_div_end_pos:next_div_pos]
-    re_div_findall = len(re.findall(r"<div", div_contents))
-    re_slashdiv_findall = len(re.findall(r"</div>", div_contents))
+    opening_element = rf'<{element_name}'
+    closing_element = rf'</{element_name}>'
+    next_element_pos = html.find(closing_element, last_div_pos)+len(closing_element)
+    start_element_end_pos = html.find('>', start_pos)+1
+    div_contents = html[start_element_end_pos:next_element_pos]
+    re_div_findall = len(re.findall(opening_element, div_contents))
+    re_slashdiv_findall = len(re.findall(closing_element, div_contents))
     if re_div_findall == re_slashdiv_findall:
-        return next_div_pos
+        return next_element_pos
     else:
-        return find_full_div(start_pos, next_div_pos)
+        return find_full_element(element_name, start_pos, next_element_pos)
+
+
+def find_full_div(start_pos, last_div_pos):
+    return find_full_element('div', start_pos, last_div_pos)
+
 
 def retrieve_div(div_id):
     global html
@@ -53,6 +64,7 @@ def retrieve_div(div_id):
     div = html[div_start_pos:div_end_pos]
     return [re.sub(r">\s*<", r"><", div), div_start_pos, div_end_pos]
 
+
 def complete_div(video, div):
     # Parse Data
     div = div.replace("[Title]", video.get("title"))
@@ -64,27 +76,54 @@ def complete_div(video, div):
     #              rf"{event.get('description')}", div)
     # div = div.replace("{{ ICS URL }}", event.get("icsURL"))
 
-    #speakers
+    # speakers
     # div = div.replace("[Speakers]", str(retrieve_speakers(event)))
-    #tags
-    div = div.replace("[Tags]", str(retrieve_tags(video)))
+    # tags
+    # div = div.replace("[Tags]", str(retrieve_tags(video)))
     div = div.replace("{{Video URL}}", video.get("videoURL"))
     div = re.sub(r"\[Description [^\]]*\]",
                  rf"{video.get('description')}", div)
     return div
 
-def complete_replace_div(videos, id, iter):
+
+def retrieve_button_tag(button_id):
     global html
-    og_div = retrieve_div(id)
-    completed_div = complete_div(videos[iter], og_div[0])
-    html = html.replace(og_div[0], completed_div)
+    button_start = re.search(rf"(<button[^>]*id=\"{button_id}\"[^>]*>)", html)
+    if button_start is None:
+        raise ValueError
+    button_start_pos = html.find(button_start.group(1))
+    button_end_pos = find_full_element('button', button_start_pos, button_start_pos)
+    if button_end_pos is None:
+        raise ValueError
+    button = html[button_start_pos:button_end_pos]
+    return [re.sub(r">\s*<", r"><", button), button_start_pos, button_end_pos]
 
-def delete_div(div_id):
+
+def complete_button_tag(tag_name, button):
+    button = button.replace("[Tags]", tag_name)
+    return button
+
+def complete_tags(video):
+    tag_buttons = retrieve_button_tag('tag-buttons')
+    tag_button = tag_buttons[0]
+    tag_names = retrieve_tags(video)
+    tags = ''
+    for tag_name in tag_names:
+        tags += complete_button_tag(tag_name, tag_button)
     global html
-    div = retrieve_div(div_id)
-    html = html.replace(div[0], '')
+    html = html.replace(tag_button, tags)
+
+# def complete_replace_div(videos, id, iter):
+#     global html
+#     og_div = retrieve_div(id)
+#     completed_div = complete_div(videos[iter], og_div[0])
+#     html = html.replace(og_div[0], completed_div)
 
 
+# def delete_div(div_id):
+#     global html
+#     div = retrieve_div(div_id)
+#     html = html.replace(div[0], '')
 
 
 def retrieve_tags(video):
@@ -100,24 +139,10 @@ def retrieve_tags(video):
                     break
     return tag_names_array
 
+
 # Returns an array containing the date and time with timezone
 def parse_date(video):
     format_string = '%Y-%m-%d'
-    # iso_date = event.get('date')
-        # iso_date = event
-    # timestamp = re.match(
-    #     r"^(\-\-|[0-9]{4}[\-]?)([0-1][0-9])[\-]?[0-3][0-9]$", iso_date)
-    # if timestamp is None:
-    #     raise ValueError(
-    #         f"Date value for event with ID {event.get('id')} is in an invalid format.")
-    # if timestamp.group(3) is not None:
-    #     format_string += ':%M'
-    # if timestamp.group(4) is not None:
-    #     format_string += ':%S'
-    # if timestamp.group(5) is not None:
-    #     format_string += ':%f'
-    # if timestamp.group(6) is None:
-    #     iso_date += '+00:00'
     return datetime.strptime(video.get('date'), format_string)
 
 
@@ -126,13 +151,12 @@ def complete_videos():
     completed_divs = ''
     for video in videos_index:
         completed_divs += complete_div(video, video_div)
+        complete_tags(video)
     global html
     html = html.replace(video_div, completed_divs)
 
 
 def videos_generation():
-    # complete_upcoming(event_index)
-    # complete_event_categories()
     complete_videos()
     open("./dist/videos.html", "w").write(html)
 
